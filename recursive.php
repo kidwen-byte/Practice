eb7c5326aa5bc6842342181d9ad52ccf95ace26c
kidwen-byte <kidwen@bk.ru>
2022-09-06 14:42:03 +0400

    recursive

---
+++ recursive.php
@@ -0,0 +1,17 @@
+<?php
+
+function find($start, $history, $target)
+{
+    if	($start	== $target)
+        echo "{$target} = {$history}<br>";
+
+    elseif ($start > $target)
+        return NULL;
+    else
+        return find($start + 5, "({$history} + 5)", $target)
+            || find($start * 3, "({$history} * 3)", $target);
+    return NULL;
+}
+
+
+find(1, "1", 48);
